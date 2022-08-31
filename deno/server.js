13fdda735fdd97895c89906912de02b7b1170f70
CB <digplan@outlook.comm>
2022-08-30 21:29:32 -0500

    0.1.0 deno and bun

--- deno/server.js
+++ deno/server.js
@@ -2,21 +2,34 @@ import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import routes from '../routes.mjs'
 console.log(routes)
 
-const s = { end: (str) => new Response(str) }
+class s {
+  end(s) {
+    this.resp = s
+  }
+}
 
 Deno.serve(async (r) => {
+
   const data = await r.text()
+  let url = new URL(r.url).pathname
+  const ru = { method: r.method, url: url }
+  const rs = new s()
+
   const midware = Object.keys(routes)
     .filter((k) => k.startsWith('_'))
-    .find((k) => routes[k](r, s, data))
+    .find((k) => routes[k](ru, rs, data))
 
-  const u = new URL(r.url).pathname.split('/')
-  if (!u[1]) u[1] = 'index.html'
-  const fn = `public/${u[1]}`
-  console.log(fn)
-  if (await existsSync(fn)) {
-    return new Response(await Deno.readTextFile(fn), {headers:{'Content-Type': 'text/html'}})
+  // Routes.mjs
+  if (routes[url]) {
+    const f = routes[url](ru, rs, data)
+    return new Response(rs.resp)
   }
 
-  return routes[`/${u[1]}`](r, { end: (str) => new Response(str) }, data)
+  // Static
+  const fn = (url == '/') ? `public/index.html` : `public/${url}`
+  if (existsSync(fn))
+    return new Response(await Deno.readTextFile(fn), { headers: { 'Content-Type': 'text/html' } })
+
+  return new Response('', { status: 404 })
+
 }, {port: 3000})
