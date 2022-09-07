d8844a362ab9c0164a0680becd55b52d0606a544
cb <digplan@outlook.com>
2022-09-06 21:30:24 -0500

    e

--- test.mjs
+++ test.mjs
@@ -2,10 +2,13 @@ import serve from './module.mjs'
 import { get, te } from '../instax/module.mjs'
 
 const server = serve({
-    '/api': (r, s) => s.end('ok')
+    '/api': (r, s, data) => {
+        s.end(data.a.toString())
+    }
 })
 
-const resp = await get('http://localhost:3000/api')
-te(resp, 'ok')
+const resp = await get('http://localhost:3000/api', {method: 'POST', body: JSON.stringify({a: 1})})
+te(resp, 1)
 
 te(server.stop(), true)
+console.log('closing server')
