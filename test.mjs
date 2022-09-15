7ff723cf2d37f3cd48a4fb504a7c6353e4d796d7
cb <digplan@outlook.com>
2022-09-15 18:06:36 -0500

    x

--- test.mjs
+++ test.mjs
@@ -16,6 +16,16 @@ tde(resp2, {a:1})
 // Public
 const testhtml = await get('http://localhost:8080/test.html')
 te(testhtml, 'ok')
-
 te(server.stop(), true)
+
+// Super shorthand
+const db = {}
+const min_server = serve({
+    c: (r, s, data) => db[data.id] = data,
+    r: (r) => db[r.url.split('/').pop()],
+    u: (r, s, data) => db[data.id] ? 'exists!' : db[data.id] = data,
+    d: (r, s, data) => db[data.id] ? delete db[data.id] : 'does not exist!',
+    q: (r, s, data) => JSON.stringify(Array.from(Object.values(db)).filter(eval(`i=>${data}`)))
+})
+serve(min_server)
 console.log('tests complete')
