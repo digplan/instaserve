a539feecd0188bf49ff7c2b836fc12f7197afbfe
cb <digplan@outlook.com>
2022-09-15 17:11:53 -0500

    fixes

--- module.mjs
+++ module.mjs
@@ -2,24 +2,31 @@ import http from 'node:http'
 import fs from 'node:fs'
 const debug = process.env.debug
 
-export default function (routes = { _debug: ({ method, url }, s) => console.log(method, url) }, port = 3000) {
+function public_file(r) {
+    if (r.url == '/') r.url = '/index.html'
+    const fn = `./public${r.url.replace('..', '')}`
+    if (fs.existsSync(fn)) {
+        if (fn.match(/.js$/)) s.writeHead(200, { 'Content-Type': 'application/javascript' })
+        return fs.readFileSync(fn, 'utf-8')
+    }
+}
 
+export default function (routes, port = 3000) {
     const server = http.createServer(async (r, s) => {
         let data = ''
         r.on('data', (s) => data += s.toString().trim())
         r.on('end', (x) => {
             try {
                 if (debug) console.log(`parsing data: "${data}"`)
-                if(data) data = JSON.parse(data)
+                if (debug) console.log(`routes: "${routes}"`)
+                if (data) data = JSON.parse(data)
                 const midware = Object.keys(routes)
                     .filter((k) => k.startsWith('_'))
                     .find((k) => routes[k](r, s, data))
-                if (r.url == '/') r.url = '/index.html'
-                const fn = `./public${r.url.replace('..', '')}`
-                if (fs.existsSync(fn)) {
-                    if (fn.match(/sw\.js/)) s.writeHead(200, { 'Content-Type': 'application/javascript' })
-                    return s.end(fs.readFileSync(fn, 'utf-8'))
-                }
+
+                const fc = public_file(r)
+                if(fc) return s.end(fc)
+                
                 const url = r.url.split('/')[1]
                 if (routes[url]) {
                     const resp = routes[url](r, s, data)
@@ -34,6 +41,7 @@ export default function (routes = { _debug: ({ method, url }, s) => console.log(
     }).listen(port)
 
     return {
+        port: port,
         server: server,
         stop: () => { server.close(); return true }
     }
--- test.mjs
+++ test.mjs
@@ -4,12 +4,18 @@ import { get, te, tde } from '../instax/module.mjs'
 const server = serve({
     api: (r, s) => 'Hello!',
     api2: (r, s, data) => JSON.stringify(data)
-}, 3001)
+}, 8080)
+te(server.port, 8080)
 
-const resp = await get('http://localhost:3001/api')
+// Routes
+const resp = await get('http://localhost:8080/api')
 te(resp, 'Hello!')
-const resp2 = await get('http://localhost:3001/api2', {method: 'POST', body: JSON.stringify({a:1})})
+const resp2 = await get('http://localhost:8080/api2', {method: 'POST', body: JSON.stringify({a:1})})
 tde(resp2, {a:1})
+
+// Public
+const testhtml = await get('http://localhost:8080/test.html')
+te(testhtml, 'ok')
 
 te(server.stop(), true)
 console.log('tests complete')
